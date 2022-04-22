cfaed61e9f1c315a232a832f40b7d8d2e011fdae
Tuna Tuncer <kuantuna.tr@gmail.com>
2022-04-22 14:38:08 +0300

    cleanup + implemented Layer class and forward method

--- hw3/layer.py
+++ hw3/layer.py
@@ -1,12 +1,28 @@
-class Layer:
-    def __init__(self):
-        self.input = None
-        self.output = None
+from typing import Callable
+import numpy as np
 
-    def forward(self, input):
-        # TODO: return the output of the layer
-        pass
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
+class Layer():
+    def __init__(self, input_size: int, output_size: int, activation: Callable = sigmoid):
+        """Number of input neurons"""
+        self.input_size: int = input_size
+        """Number of output neurons"""
+        self.output_size: int = output_size
+
+        """shape: input_size x output_size"""
+        self.weights = np.random.randn(input_size, output_size)
+        """shape: output_size x 1"""
+        self.bias = np.random.randn(output_size, 1)
+
+        self.activation: Callable = activation
+
+    def forward(self, input) -> np.ndarray:
+        """Expects input to be a numpy array of size input_size x 1 and returns a numpy array of size output_size x 1"""
+        return self.activation(np.matmul(self.weights.T, input) + self.bias)
 
     def backward(self, output_gradient, learning_rate):
-        # TODO: update the weights and bias using the output gradient and the learning rate
-        pass
+        raise NotImplementedError()
